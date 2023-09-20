d862642b330bdaf3eca11d122632d218533442ff
Billy19191 <bill.ratchanon@gmail.com>
2023-09-20 20:58:59 +0700

    feat: clear and fix all unmatch name rpgf1

--- index5.js
+++ index5.js
@@ -1,9 +1,6 @@
-import data1x from './csvjson.json' assert { type: 'json' }
-import data2x from './csvjson-2.json' assert { type: 'json' }
+import data1 from './result_rpgf1/rpgf1_1.json' assert { type: 'json' }
+import data2 from './result_rpgf1/rpgf1_2.json' assert { type: 'json' }
 import fs from 'fs'
-
-const data1 = data1x
-const data2 = data2x
 
 const matchedData = []
 const unmatchedData = []
@@ -11,18 +8,17 @@ const unmatchedData = []
 for (let each of data1) {
   let count = 0
   for (let each2 of data2) {
-    if (each.name === each2.name) {
+    if (each['Project Name'] === each2.name) {
       count++
       const combinedData = { ...each, allocation: each2.allocation.slice(1) }
       matchedData.push(combinedData)
     }
   }
   if (count === 0) {
-    unmatchedData.push(each.name)
+    unmatchedData.push(each['Project Name'])
   }
 }
 
-fs.writeFileSync(`./results_rpgf1.json`, JSON.stringify(matchedData), 'utf-8')
-
-// console.log('Matched Data:', matchedData)
 // console.log('Unmatched Data:', unmatchedData)
+// console.log('Unmatched Data:', unmatchedData.length)
+fs.writeFileSync(`./results_rpgf1.json`, JSON.stringify(matchedData), 'utf-8')
